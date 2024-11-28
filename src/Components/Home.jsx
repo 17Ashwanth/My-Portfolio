b8881afdea02405ff34daee50ee73e24b5372e0f
17Ashwanth <ashwanthcheleri@gmail.com>
2024-11-28 19:55:05 +0530

    Resume path changed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 
+
 function Home() {
   return (
     <>
@@ -8,7 +9,7 @@ function Home() {
         <div className="text-center">
           <h1 style={{ fontFamily: "Anta, sans-serif", fontWeight: "800", fontStyle: "normal" }}  className="mb-2">I AM A <span className='text-primary'>ME(A)RN</span> STACK DEVELOPER</h1>
           <h3 style={{ fontFamily: "Anta, sans-serif", fontWeight: "500", fontStyle: "normal" }} >This is my Official Portfolio Website <br /> to Describe all Details and Work Experience <br /> in Web Development.</h3>
-          <a href='https://drive.google.com/file/d/1ax-8734aM5tjrZRjC9q-uFBmXAMv3wXf/view?usp=drive_link' target='_blank'><button  className='btn btn-primary mt-2'>GET THE RESUME{" "}<i class="fa-solid fa-download fa-bounce"></i></button></a>
+          <a href="/Ashwanth.pdf" download="Ashwanth_Resume.pdf" target='_blank'><button  className='btn btn-primary mt-2'>GET THE RESUME{" "}<i class="fa-solid fa-download fa-bounce"></i></button></a>
         </div>
       </section>
     </>
